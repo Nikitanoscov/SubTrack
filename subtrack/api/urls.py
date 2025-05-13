19d952176cfc9be8cc4fb6a5430a558632c8d2cb
Nikita Noskov <nik030205@mail.ru>
2025-05-13 17:49:05 +0300

    Register user url for register, set password, create jwt and refresh jwt

--- subtrack/api/urls.py
+++ subtrack/api/urls.py
@@ -1,6 +1,11 @@
+from django.contrib.auth import authenticate, get_user_model
 from django.contrib import admin
 from django.urls import include, path
+from djoser.views import UserViewSet
+
 
 urlpatterns = [
+    path('users/register/', UserViewSet.as_view({'post': 'create'}), name='register'),
+    path('users/set_password/', UserViewSet.as_view({'post': 'set_password'}), name='set-password'),
     path('', include('djoser.urls.jwt')),
 ]
