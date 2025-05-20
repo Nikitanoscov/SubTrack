f73c4d1c47201342936ac0458a1a86b9965a98f9
Nikita Noskov <nik030205@mail.ru>
2025-05-20 17:06:39 +0300

    Update urls list for user

--- subtrack/users/urls.py
+++ subtrack/users/urls.py
@@ -4,17 +4,17 @@ from djoser.views import UserViewSet
 
 urlpatterns = [
     path(
-        'users/register/',
+        'register/',
         UserViewSet.as_view({'post': 'create'}),
         name='register'
     ),
     path(
-        'users/set_password/',
+        'set_password/',
         UserViewSet.as_view({'post': 'set_password'}),
         name='set-password'
     ),
     path(
-        'users',
+        '',
         include('djoser.urls.jwt')
     ),
 ]
