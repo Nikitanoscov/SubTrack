c0fcb3ad1589b0106d0d4cfb7a2cf9bbb1e1087c
Nikita Noskov <nik030205@mail.ru>
2025-05-22 21:35:10 +0300

    Delete trash.

--- subtrack/api/serializers.py
+++ subtrack/api/serializers.py
@@ -1,10 +1,1 @@
-from rest_framework import serializers
 
-from subscriptions.models import SubscriptionsTypes
-
-
-class TypesSerializers(serializers.ModelSerializer):
-
-
-    class Meta:
-        model = 
