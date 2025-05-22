8f6239223cd524ac1b759a87921053b646c21b88
Nikita Noskov <nik030205@mail.ru>
2025-05-22 22:15:06 +0300

    Create viewset for crud to subscriptions.

--- subtrack/subscriptions/views.py
+++ subtrack/subscriptions/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, viewsets
 from rest_framework.decorators import api_view
 
 from .models import SubscriptionsTypes, Subscriptions
-from .serializers import TypesSerializer
+from .serializers import (
+    TypesSerializer,
+    SubscriptionsDetailSerializer,
+    SubscriptionsListSerializer,
+    SubscriptionsUpdateSerializer
+)
 
 
 @api_view(('GET',))
@@ -24,3 +29,28 @@ def status_list(request):
 class TypesListView(ListAPIView):
     queryset = SubscriptionsTypes.objects.all()
     serializer_class = TypesSerializer
+
+
+class SubscriptionsViewSet(viewsets.ModelViewSet):
+    queryset = Subscriptions.objects.all()
+    serializer_class = SubscriptionsDetailSerializer
+
+    def get_serializer_class(self, *args, **kwargs):
+        if self.action in ('partial_update'):
+            return SubscriptionsUpdateSerializer
+        elif self.action in ('list'):
+            return SubscriptionsListSerializer
+        return super().get_serializer(*args, **kwargs)
+
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(
+            instance,
+            data=request.data,
+            partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        updated_instance = serializer.save()
+        detail_serializer = SubscriptionsDetailSerializer(updated_instance)
+        return Response(detail_serializer.data, status=status.HTTP_200_OK)
+
